cbaed87504abddbf859ce3c9d2fd41cd877ad6e4
dilakv <ingfvalencia@gmail.com>
2022-05-08 21:21:27 -0500

    Testing change

--- go/01-NumberLineJumps.go
+++ go/01-NumberLineJumps.go
@@ -19,7 +19,7 @@ import (
  *  3. INTEGER x2
  *  4. INTEGER v2
  */
-// https://www.hackerrank.com/challenges/kangaroo/problem
+//  https://www.hackerrank.com/challenges/kangaroo/problem
 func kangaroo(x1 int32, v1 int32, x2 int32, v2 int32) string {
     // Write your code here
     for x1, x2;x1 <= x2; x1 += v1, x2 += v2{
